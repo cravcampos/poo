91302882c712c0b718895b19fb29545ba51c6d2b
cravcampos <cravcampos@gmail.com>
2022-02-05 14:20:20 -0500

    Atributos y métodos estáticos de la clase

---
+++ src/seccion14_clasesyobjetos/_14_atributosymetodosestaticos/Automovil.java
@@ -0,0 +1,174 @@
+package seccion14_clasesyobjetos._14_atributosymetodosestaticos;
+
+public class Automovil {
+
+    // Atributos de la clase
+    private String fabricante;
+    private String modelo;
+    private String color;
+    private double cilindrada;
+    private int capacidadTanque = 40;
+    private static String colorPatente = "Rojo";
+
+    // Método constructor vacío
+    public Automovil(){
+
+    }
+
+    // Método constructor con un parámetros
+    public Automovil(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    // Método constructor con dos parámetros
+    public Automovil(String fabricante, String modelo){
+        this(fabricante);
+        this.modelo = modelo;
+    }
+
+    public Automovil(String fabricante, String modelo, String color) {
+        this(fabricante, modelo);
+        this.color = color;
+    }
+
+    public Automovil(String fabricante, String modelo, String color, double cilindrada) {
+        this(fabricante, modelo, color);
+        this.cilindrada = cilindrada;
+    }
+
+    public Automovil(String fabricante, String modelo, String color, double cilindrada, int capacidadTanque) {
+        this(fabricante, modelo, color, cilindrada);
+        this.capacidadTanque = capacidadTanque;
+    }
+
+
+    // Métodos para leer los atributos
+    public String getFabricante(){
+        return this.fabricante;
+    }
+
+    public String getModelo(){
+        return this.modelo;
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+
+    public double getCilindrara(){
+        return  this.cilindrada;
+    }
+
+    public int getCapacidadTanque(){
+        return this.capacidadTanque;
+    }
+
+    public static String getColorPatente() {
+        return colorPatente;
+    }
+
+    public static void setColorPatente(String colorPatente) {
+        Automovil.colorPatente = colorPatente;
+    }
+
+    // Métodos para modificar los atributos
+    public void setFabricante(String fabricante){
+        this.fabricante = fabricante;
+    }
+
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public void setCilindrada(double cilindrada){
+        this.cilindrada = cilindrada;
+    }
+
+    public void setCapacidadTanque(int capacidadTanque){
+        this.capacidadTanque = capacidadTanque;
+    }
+
+    //Métodos en la clase
+    public void detalle(){
+        System.out.println("auto.fabricante = " + this.fabricante);
+        System.out.println("auto.modelo = " + this.modelo);
+        System.out.println("auto.color = " + this.color);
+        System.out.println("auto.cilindrada = " + this.cilindrada);
+    }
+
+    public String detalle2(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("auto.fabricante = " + this.fabricante);
+        sb.append("\nauto.modelo = " + this.modelo);
+        sb.append("\nauto.color = " + this.color);
+        sb.append("\nauto.cilindrada = " + this.cilindrada);
+        return sb.toString();
+    }
+
+    public String detalle3(){
+        return "auo.fabricante = " + this.getFabricante() +
+                "\nauto.modelo = " + this.getModelo() +
+                "\nauto.color = " + this.getColor() +
+                "\nauto.patenteColor = " + colorPatente +
+                "\nauto.cilindrada = " + this.getCilindrara();
+    }
+
+    public String acelerar(int rpm){
+        return "El automóvil " + this.modelo + " acelerando a " + rpm + " rpm";
+    }
+
+    public String frenar(){
+        return this.fabricante + " " + this.modelo + " frenando";
+    }
+
+    // pasar una combinación de métodos
+    public String acelerarFrenar(int rpm){
+        String acelerar = this.acelerar(rpm);
+        String frenar = this.frenar();
+        return acelerar + "\n" + frenar;
+    }
+
+    public float calcularConsumo(int km, float porcentajeGasolina){
+        return km/(capacidadTanque * porcentajeGasolina);
+    }
+
+    // Ejemplo de sobrecarga de métodos mismo nombre diferentes tipos de dato en los parámetros
+    public float calcularConsumo(int km, int porcentajeGasolina){
+        return km/(capacidadTanque * (porcentajeGasolina/100f));
+    }
+
+    @Override
+    public String toString() {
+        return "Automovil{" +
+                "fabricante='" + fabricante + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", color='" + color + '\'' +
+                ", cilindrada=" + cilindrada +
+                ", capacidadTanque=" + capacidadTanque +
+                '}';
+    }
+
+    // Sobreescribiendo el método equals para comparar por fabricante y modelo
+    @Override
+    public boolean equals(Object obj) {
+        //Si ambos son iguales por referencia retornamos true
+        if (this == obj){
+            return true;
+        }
+        // Si la instancia del objeto es diferente a automóvil se sale de método y retorna false en caso de que queramos comparar
+        // con otro objeto de diferente instancia
+        if (!(obj instanceof Automovil)){
+            return false;
+        }
+        Automovil a = (Automovil) obj;
+        return (this.fabricante != null && this.modelo == null // validando que los datos ingresados en el objeto no sean nullos
+                && this.fabricante.equalsIgnoreCase(a.getFabricante())
+                && this.modelo.equalsIgnoreCase(a.getModelo()));
+
+
+    }
+}
---
+++ src/seccion14_clasesyobjetos/_14_atributosymetodosestaticos/Principal.java
@@ -0,0 +1,72 @@
+package seccion14_clasesyobjetos._14_atributosymetodosestaticos;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        // creación de un objeto a partir de un constructor vacío
+        Automovil auto1 = new Automovil();
+        auto1.setFabricante("Renault");
+        auto1.setModelo("Duster 4 x 4");
+        auto1.setColor("Gris");
+        auto1.setCilindrada(2.0);
+        auto1.setCapacidadTanque(5);
+
+        // creación de un objeto a partir de un constructor con un argumento
+        Automovil auto2 = new Automovil("Mazda");
+        auto2.setModelo("626 Matsuri");
+        auto2.setColor("Estrato perla");
+        auto2.setCilindrada(2.0);
+
+        // creación de un objeto a partir de un constructor con dos argumentos
+        Automovil auto3 = new Automovil("Dodge","Dodge 100 cabina de 12 pasajeros");
+        auto3.setColor("Rojo Blanco");
+        auto3.setCilindrada(2.5);
+
+        // creación de un objeto a partir de un constructor con tres argumentos
+        Automovil auto4 = new Automovil("TVS","Apache 200 RTR 4 Válvulas", "Negro Mate");
+        auto4.setCilindrada(200);
+
+        // creación de un objeto a partir de un constructor con cuatro argumentos
+        Automovil auto5 = new Automovil("TVS","Apache 180 RTR 2 Válvulas", "Azul Mate", 180);
+
+        // creación de un objeto a partir de un constructor con cinco argumentos
+        Automovil auto6 = new Automovil("Nissan","Sentra","Rojo", 1.6,3);
+
+        // creación de un objeto a partir de un constructor con todos los argumentos
+        Automovil auto7 = new Automovil("TVS","Apache 160 RTR 2 Válvulas", "Rojo Negro", 160);
+
+        Automovil auto8 = new Automovil("TVS","Apache 160 RTR 2 Válvulas", "Rojo Negro", 160);
+
+        Automovil auto9 = new Automovil();
+
+        // Accediendo al atributo estático de la clase para modificar el colorPatente en todos los objetos creados
+        Automovil.setColorPatente("Gris");
+
+        System.out.println("===== Auto 1  ======");
+        System.out.println(auto1.detalle3());
+
+        System.out.println();
+        System.out.println("===== Auto 2  ======");
+        System.out.println(auto2.detalle3());
+
+        System.out.println();
+        System.out.println("===== Auto 3  ======");
+        System.out.println(auto3.detalle3());
+
+        System.out.println();
+        System.out.println("===== Auto 4  ======");
+        System.out.println(auto4.detalle3());
+
+        System.out.println();
+        System.out.println("===== Auto 5  ======");
+        System.out.println(auto5.detalle3());
+
+        System.out.println();
+        System.out.println("===== Auto 6  ======");
+        System.out.println(auto6.detalle3());
+
+        System.out.println();
+        System.out.println("===== Auto 7  ======");
+        System.out.println(auto7.detalle3());
+    }
+}
