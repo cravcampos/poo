9750f3acc300105a9e96281d07e5ec60890b6af3
cravcampos <cravcampos@gmail.com>
2022-02-05 00:27:23 -0500

    Se crea la clase Automovil con cuatro atributos

---
+++ src/seccion14_clasesyobjetos/_01_primeraclase/Automovil.java
@@ -0,0 +1,11 @@
+package seccion14_clasesyobjetos._01_primeraclase;
+
+public class Automovil {
+
+    // Atributos de la clase
+    String fabricante;
+    String modelo;
+    String color;
+    double cilindrada;
+
+}
---
+++ src/seccion14_clasesyobjetos/_01_primeraclase/Principal.java
@@ -0,0 +1,7 @@
+package seccion14_clasesyobjetos._01_primeraclase;
+
+public class Principal {
+    public static void main(String[] args) {
+
+    }
+}
