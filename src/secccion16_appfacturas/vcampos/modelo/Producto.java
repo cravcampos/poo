30784efb623731e228a770da61db72b12c9ce7ad
cravcampos <cravcampos@gmail.com>
2022-02-08 23:41:16 -0500

    Creando proyecto ejemplo facturas, sus clases, sus getter and setter, sus constructores y las diferentes relaciones

---
+++ src/secccion16_appfacturas/vcampos/modelo/Producto.java
@@ -0,0 +1,33 @@
+package secccion16_appfacturas.vcampos.modelo;
+
+public class Producto {
+
+    private int codigo;
+    private String nombre;
+    private double precio;
+    private static int ultimoCodigo;
+
+    public Producto() {
+        this.codigo = ++ultimoCodigo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
